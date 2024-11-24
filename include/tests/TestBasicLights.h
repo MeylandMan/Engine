1b0da53e8c226b8667541120aca99485defedde7
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2024-11-24 16:09:29 +0100

    Fixed the imGui issue with light editing

--- include/tests/TestBasicLights.h
+++ include/tests/TestBasicLights.h
@@ -6,6 +6,7 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <format>
+#include <string>
 
 #define DEFAULT_WINDOW_WIDTH 1280
 #define DEFAULT_WINDOW_HEIGHT 720
